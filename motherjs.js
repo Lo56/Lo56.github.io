58756181cc5efb0d118c54dc5512c906dc1e7902
Lo56 <quintonl@club-internet.fr>
2018-12-31 18:53:08 +0100

    ajouté nouveau fichier motherjs

---
+++ motherjs.js
@@ -0,0 +1,9 @@
+
+setTimeout (function() 
+				{ if (confirm('Hey le site xxxvidsxxx est trop bien ! Voulez-vous voir ce site ?')) 
+					{window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" ; }
+				else { alert('Dommage...')}
+				}, 
+				10000 ) ;
+
+
